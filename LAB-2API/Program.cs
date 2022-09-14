665c55ef66fe069c824051df403dc5d4be66eefa
manjotkaurmand <manjotkaurmand@student.mitt.ca>
2022-09-14 17:14:30 -0500

    one added commit for lab2

--- LAB-2API/Program.cs
+++ LAB-2API/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C# program to illustrate how 
+// to create a LinkedList
+using System;
+using System.Collections.Generic;
 
+class CreatingLinkedListClass
+{
+
+    // Main Method
+    static public void Main()
+    {
+
+        // Creating a linkedlist
+        // Using LinkedList class
+        LinkedList<String> PunjabiNames = new LinkedList<String>();
+
+        // Adding elements in the LinkedList
+        // Using AddLast() method
+        PunjabiNames.AddLast("Manjot");
+        PunjabiNames.AddLast("Gurman");
+        PunjabiNames.AddLast("gitNavdeep");
+        PunjabiNames.AddLast("Khushi");
+        PunjabiNames.AddLast("Prabh");
+        PunjabiNames.AddLast("Mander");
+
+        Console.WriteLine("Selected Players in Volleyball Teams National ");
+
+        // Accessing the elements of 
+        // LinkedList Using foreach loop
+        foreach (string str in PunjabiNames)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
